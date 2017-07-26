a8f7b2c4357cf6573de320757bf84cf8a1d31f1b
Gideon <gid.pfeffer@gmail.com>
2017-07-26 01:44:31 +0000

    added routes for edit, update, show, and index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,25 @@
 class ArticlesController < ApplicationController
   
+  def index 
+    @articles = Article.all
+  end
+  
   def new
     @article = Article.new
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
   
   def create
@@ -11,7 +29,7 @@ class ArticlesController < ApplicationController
       flash[:notice]= "Article was successfully saved"
       redirect_to article_path(@article)
     else
-      render :new
+      render 'new'
     end
   end
   
